3d9fb584fc18e8922659a7f93da046afbb4631fc
Mavdavis <ekwesimarvelous@gmail.com>
2025-06-21 14:49:21 +0100

    fund me

--- test/Fundme.t.sol
+++ test/Fundme.t.sol
@@ -5,10 +5,14 @@ import {Test, console} from "forge-std/Test.sol";
 import{DeployFundMe} from "../script/DeployFundMe.s.sol";
 import {FundMe} from "../src/Fundme.sol";
 contract FundMeTest  is Test{
+        address USER = makeAddr("user");
+        uint256 constant STARTING_BALANCE = 10 ether; 
+        uint256 constant SEND_VALUE = 1 ether;
          FundMe fundMe;
     function setUp() public {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
     function testMinimumDollarIsFive() public view{
         assertEq(fundMe.MINIMUM_USD(),5e18);
@@ -19,5 +23,15 @@ contract FundMeTest  is Test{
     function testIfAgregatorV3InterfaceVersionIsCorrect() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+    function testFunctionFailsWithoutEnoughFunds()public{
+        vm.expectRevert();
+        fundMe.fund();        
+    }
+    function testFundFunctionUpdatesFundedDataStructure() public  {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddessToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
